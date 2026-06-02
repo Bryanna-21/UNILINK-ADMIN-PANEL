50e62c81670686b450fc5b7736b0eb160d9c78be
Bryanna-21 <bryannamanu@gmail.com>
2026-06-02 08:45:46 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import AuthProvider from "./context/AuthContext";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+
+import ProtectedRoute from "./components/ProtectedRoute";
+
+export default function App() {
+  return (
+    <AuthProvider>
+
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route
+            path="/"
+            element={<Login />}
+          />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+        </Routes>
+
+      </BrowserRouter>
+
+    </AuthProvider>
+  );
+}
