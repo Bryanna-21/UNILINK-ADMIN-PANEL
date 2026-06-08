b2aa04f4481135bb1357966d1cd4b2c35a294d90
Bryanna-21 <bryannamanu@gmail.com>
2026-06-08 18:54:38 +0300

    Create student.ts

---
+++ src/types/student.ts
@@ -0,0 +1,17 @@
+export interface Student {
+  id: string;
+
+  name: string;
+
+  email: string;
+
+  university: string;
+
+  course: string;
+
+  year: number;
+
+  status: string;
+
+  avatar?: string;
+}
