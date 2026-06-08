00911bf7132f11e79f52e31efdac5336812a8111
Bryanna-21 <bryannamanu@gmail.com>
2026-06-08 18:42:09 +0300

    Create token-manager.ts

---
+++ src/lib/token-manager.ts
@@ -0,0 +1,16 @@
+let accessToken: string | null =
+  null;
+
+export function setAccessToken(
+  token: string
+) {
+  accessToken = token;
+}
+
+export function getAccessToken() {
+  return accessToken;
+}
+
+export function clearAccessToken() {
+  accessToken = null;
+}
